b8908860d351495f9b3348cf135c50f4e993f0a3
abraao <abraao@kali-abraao.dev>
2025-09-22 16:34:10 -0300

    Adicionado linha 1 para execução em sistemas Linux

--- src/senha_segura.py
+++ src/senha_segura.py
@@ -1,12 +1,12 @@
+#!/usr/bin/env python3
+
 # TODO: Trocar o módulo random por secrets, que é o módulo especilizado para 
 # geração de números aleátorios seguros.
 from random import randrange
 from sys import argv
 
-
 TAM_MINIMO = 8
 TAM_MAXIMO = 16
-
 
 caracteres = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'w', 'y', 'z', 
@@ -14,7 +14,6 @@ caracteres = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
               'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'W', 'Y', 'Z', 
               '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '@', '%', '#', 
               '&', '$', '!')
-
 
 def gerar_senha_segura(tam_senha=16):
     """Gera senhas seguras de 8 e 16 caracteres."""
@@ -29,7 +28,6 @@ def gerar_senha_segura(tam_senha=16):
             senha += caracteres[index]    
 
     return senha
-
 
 senha_segura = gerar_senha_segura(int(argv[1]))
 print(senha_segura)
